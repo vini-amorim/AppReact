b1cbd955fc3b7740912e3213263a2829b24b1f55
Vinicius Amorim <viinii.amorim@gmail.com>
2024-03-26 19:55:28 -0300

    challenge-react-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,87 @@
 import React from 'react';
-import Input from './Form/Input';
-import Select from './Form/Select';
 import Radio from './Form/Radio';
 
+const perguntas = [
+  {
+    pergunta: 'Qual método é utilizado para criar componentes?',
+    options: [
+      'React.makeComponent()',
+      'React.createComponent()',
+      'React.createElement()',
+    ],
+    resposta: 'React.createElement()',
+    id: 'p1',
+  },
+  {
+    pergunta: 'Como importamos um componente externo?',
+    options: [
+      'import Component from "./Component"',
+      'require("./Component")',
+      'import "./Component"',
+    ],
+    resposta: 'import Component from "./Component"',
+    id: 'p2',
+  },
+  {
+    pergunta: 'Qual hook não é nativo?',
+    options: ['useEffect()', 'useFetch()', 'useCallback()'],
+    resposta: 'useFetch()',
+    id: 'p3',
+  },
+  {
+    pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
+    options: ['set', 'get', 'use'],
+    resposta: 'use',
+    id: 'p4',
+  },
+];
+
 const App = () => {
-  const [nome, setNome] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [produto, setProduto] = React.useState('');
-  const [cor, setCor] = React.useState('');
+  const [respostas, setRespostas] = React.useState({
+    p1: '',
+    p2: '',
+    p3: '',
+    p4: '',
+  });
+  const [slide, setSlide] = React.useState(0);
+  const [resultado, setResultado] = React.useState(null);
+
+  function handleChange({ target }) {
+    setRespostas({ ...respostas, [target.id]: target.value });
+  }
+
+  function resultadoFinal() {
+    const corretas = perguntas.filter(
+      ({ id, resposta }) => respostas[id] === resposta,
+    );
+    setResultado(`Você acertou: ${corretas.length} de ${perguntas.length}`);
+  }
+
+  function handleClick() {
+    if (slide < perguntas.length - 1) {
+      setSlide(slide + 1);
+    } else {
+      setSlide(slide + 1);
+      resultadoFinal();
+    }
+  }
+
   return (
-    <form>
-      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
-      <Select
-        options={[
-          'Smartphone',
-          'Tablet',
-          'Notebook',
-          'Cadeira',
-          'Mouse',
-          'Teclado',
-        ]}
-        value={produto}
-        setValue={setProduto}
-      />
-      <Input id="nome" label="Nome" value={nome} setValue={setNome} />
-      <Input id="email" label="Email" value={email} setValue={setEmail} />
+    <form onSubmit={(event) => event.preventDefault()}>
+      {perguntas.map((pergunta, index) => (
+        <Radio
+          active={slide === index}
+          key={pergunta.id}
+          value={respostas[pergunta.id]}
+          onChange={handleChange}
+          {...pergunta}
+        />
+      ))}
+      {resultado ? (
+        <p>{resultado}</p>
+      ) : (
+        <button onClick={handleClick}>Próxima</button>
+      )}
     </form>
   );
 };
--- src/Form/Radio.jsx
+++ src/Form/Radio.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 
-const Radio = ({ options, value, setValue }) => {
+const Radio = ({ pergunta, options, onChange, value, id, active }) => {
+  if (active === false) return null;
   return (
-    <>
+    <fieldset
+      style={{
+        padding: '2rem',
+        marginBottom: '1rem',
+        border: '2px solid #eee',
+      }}
+    >
+      <legend style={{ fontWeight: 'bold' }}>{pergunta}</legend>
       {options.map((option) => (
-        <label key={option}>
+        <label
+          key={option}
+          style={{ marginBottom: '1rem', fontFamily: 'monospace' }}
+        >
           <input
             type="radio"
-            value={option}
+            id={id}
             checked={value === option}
-            onChange={({ target }) => setValue(target.value)}
+            value={option}
+            onChange={onChange}
           />
           {option}
         </label>
       ))}
-    </>
+    </fieldset>
   );
 };
 
