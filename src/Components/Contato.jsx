d00e5c9d7e0bacd9e6ae28e90afae0f3822e0c0a
Vinicius Amorim <viinii.amorim@gmail.com>
2024-04-04 00:23:33 -0300

    initial configuration

---
+++ src/Components/Contato.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Styles from './Contato.module.css';
+
+const Contato = () => {
+  return <div>Contato</div>;
+};
+
+export default Contato;
---
+++ src/Components/Produto.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Styles from './Produto.module.css';
+
+const Produto = () => {
+  return <div>Produto</div>;
+};
+
+export default Produto;
